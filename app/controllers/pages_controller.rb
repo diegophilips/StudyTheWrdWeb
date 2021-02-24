19550edd6c1bbdbcc8d2fd8a9daedfb7696d3cb6
Diego Philips <diego.r.philips@gmail.com>
2021-02-24 17:04:10 +0000

    added bunch of stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,4 +15,54 @@ class PagesController < ApplicationController
 
   def policy
   end
+
+  def equal
+    send_file 'app/assets/docs/TheWrd-Policy-01-Equal-Opportunities-Policy.pdf', :type => 'application/pdf', :disposition => 'attachment'
+  end
+
+  def induction
+    send_file 'app/assets/docs/TheWrd-Policy-02-Induction-and-Enrolment.pdf', :type => 'application/pdf', :disposition => 'attachment'
+  end
+
+  def registration
+    send_file 'app/assets/docs/TheWrd-Policy-03-Registration-_-Certification-Policy.pdf', :type => 'application/pdf', :disposition => 'attachment'
+  end
+
+  def learning
+    send_file 'app/assets/docs/TheWrd-Policy-04-Learning-_-Teaching-Strategy.pdf', :type => 'application/pdf', :disposition => 'attachment'
+  end
+
+  def complaints
+    send_file 'app/assets/docs/TheWrd-Policy-05-Complaints-_-Appeals-Policy.pdf', :type => 'application/pdf', :disposition => 'attachment'
+  end
+
+  def internal
+    send_file 'app/assets/docs/TheWrd-Policy-06-Internal-Moderation-Policy.pdf', :type => 'application/pdf', :disposition => 'attachment'
+  end
+
+  def academic
+    send_file 'app/assets/docs/TheWrd-Policy-07-Academic-Malpractice-Policy.pdf', :type => 'application/pdf', :disposition => 'attachment'
+  end
+
+  def health
+    send_file 'app/assets/docs/TheWrd-Policy-09-Health-_-Safety.pdf', :type => 'application/pdf', :disposition => 'attachment'
+  end
+
+  def quality
+    send_file 'app/assets/docs/TheWrd-Policy-10-Quality_Assurance.pdf', :type => 'application/pdf', :disposition => 'attachment'
+  end
+
+  def verification
+    send_file 'app/assets/docs/TheWrd-Policy-11-Internal_Verification.pdf', :type => 'application/pdf', :disposition => 'attachment'
+  end
+
+  def conflict
+    send_file 'app/assets/docs/TheWrd-Policy-12-Conflict-of-interest.pdf', :type => 'application/pdf', :disposition => 'attachment'
+  end
+
+  def rpl
+    send_file 'app/assets/docs/TheWrd-Policy-13-RPL.pdf', :type => 'application/pdf', :disposition => 'attachment'
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,18 @@ Rails.application.routes.draw do
   get :terms, to: "pages#terms"
   get :privacy, to: "pages#privacy"
   get :policy, to: "pages#policy"
+  get :equal, to: "pages#equal"
+  get :induction, to: "pages#induction"
+  get :registration, to: "pages#registration"
+  get :learning, to: "pages#learning"
+  get :complaints, to: "pages#complaints"
+  get :internal, to: "pages#internal"
+  get :academic, to: "pages#academic"
+  get :health, to: "pages#health"
+  get :quality, to: "pages#quality"
+  get :verification, to: "pages#verification"
+  get :conflict, to: "pages#conflict"
+  get :rpl, to: "pages#rpl"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
